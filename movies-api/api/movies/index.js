fa1a5770b574c7e181e3799db889df66fc991c70
adam cahill <20097733@mail.wit.ie>
2023-12-22 19:39:03 +0000

    Added full api support for react app

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,7 +1,7 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
-import {getUpcomingMovies, getGenres} from '../tmdb-api';
+import {getUpcomingMovies, getGenres, getMovies, getNowPlaying, getTopRated, searchMovies, getMovieImages, getMovie, getMovieReviews, getWatchProviders, getMovieTrailers} from '../tmdb-api';
 
 const router = express.Router();
 
@@ -37,11 +37,74 @@ router.get('/:id', asyncHandler(async (req, res) => {
     }
 }));
 
+
+
+router.get('/tmdb/movies', asyncHandler(async (req, res) => {
+    let { page = 1} = req.query;
+    const movies = await getMovies(page);
+    
+    res.status(200).json(movies);
+}));
+
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     let { page = 1} = req.query;
     const upcomingMovies = await getUpcomingMovies(page);
     
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/tmdb/nowPlaying', asyncHandler(async (req, res) => {
+    let { page = 1} = req.query;
+    const nowPlayingMovies = await getNowPlaying(page);
+    
+    res.status(200).json(nowPlayingMovies);
+}));
+
+router.get('/tmdb/topRated', asyncHandler(async (req, res) => {
+    let { page = 1} = req.query;
+    const topRatedMovies = await getTopRated(page);
+    
+    res.status(200).json(topRatedMovies);
+}));
+
+router.get('/tmdb/searchMovies', asyncHandler(async (req, res) => {
+    let search = req.query.search;
+    const searchMovie = await searchMovies(search);
+    
+    res.status(200).json(searchMovie);
+}));
+
+router.get('/tmdb/movieImages/:id', asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const movieImages = await getMovieImages({ queryKey: ["images", { id }] });
+    res.status(200).json(movieImages);
+}));
+
+router.get('/tmdb/movie/:id', asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const movieDetails = await getMovie({ queryKey: ["movie", { id }] });
+    res.status(200).json(movieDetails);
+}));
+
+router.get('/tmdb/reviews', asyncHandler(async (req, res) => {
+    let id = req.query.id;
+    const movieReviews = await getMovieReviews(id);
+    
+    res.status(200).json(movieReviews);
+}));
+
+router.get('/tmdb/watchProviders', asyncHandler(async (req, res) => {
+    let id = req.query.id;
+    const movieProviders = await getWatchProviders(id);
+    
+    res.status(200).json(movieProviders);
+}));
+
+router.get('/tmdb/trailer', asyncHandler(async (req, res) => {
+    let id = req.query.id;
+    const movieTrailer = await getMovieTrailers(id);
+    
+    res.status(200).json(movieTrailer);
 }));
 
 router.get('/tmdb/genres', asyncHandler(async (req, res) => {
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,5 +1,21 @@
 import fetch from 'node-fetch';
 
+export const getMovies = (page) => {
+  return fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+
 export const getUpcomingMovies = async (page) => {
     try {
         const response = await fetch(
@@ -14,6 +30,119 @@ export const getUpcomingMovies = async (page) => {
     } catch (error) {
         throw error;
     }
+};
+
+export const getNowPlaying = (page) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
+export const getTopRated = (page) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
+export const searchMovies = async (query) => {
+  return fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_KEY}&language=en-US&query=${query}&include_adult=false`
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.json().message);
+    }
+    return response.json();
+  })
+  .catch((error) => {
+     throw error
+  });
+};
+
+export const getMovieImages = async ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.TMDB_KEY}`
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    return response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+export const getMovie = async ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}`
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    return response.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getMovieReviews = (id) => {
+  return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.TMDB_KEY}`
+  )
+      .then((res) => res.json())
+      .then((json) => {
+          return json.results;
+      });
+};
+
+export const getWatchProviders = (id) => {
+  return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${process.env.TMDB_KEY}`
+  )
+      .then((res) => res.json())
+      .then((json) => {
+          return json.results;
+      });
+};
+
+export const getMovieTrailers = (id) => {
+  return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${process.env.TMDB_KEY}`
+  )
+      .then((res) => res.json())
+      .then((json) => {
+          return json.results;
+      });
 };
 
 export const getGenres = async () => {
